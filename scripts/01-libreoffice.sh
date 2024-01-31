330e875c5b0d5ba02f39135506194c4f8f0dc28f
AJ ONeal <aj@therootcompany.com>
2024-01-31 15:41:45 -0700

    doc: unoconv is now unoserver

--- scripts/01-libreoffice.sh
+++ scripts/01-libreoffice.sh
@@ -6,7 +6,7 @@ my_version="6-4"
 
 if [[ -z "$(command -v libreoffice)" ]]; then
     # TODO make this a microservice
-    # TODO investigate unoconv, see https://askubuntu.com/a/398499
+    # TODO investigate unoconv (now unoserver), see https://askubuntu.com/a/398499
     export DEBIAN_FRONTEND=noninteractive
 
     sudo add-apt-repository -y ppa:libreoffice/ppa
